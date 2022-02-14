adf01ba66ac73b05093b0692e77cd4b63a812939
Théo Rosset <theo.rosset@outlook.fr>
2022-02-14 17:18:14 +0100

    possibilité de modifier un status

--- back/Controllers/post.js
+++ back/Controllers/post.js
@@ -71,9 +71,41 @@ const deletePost = (req, res, next) => {
     }
   });
 };
-
+/**
+ * mise a jours d'un post
+ */
 const updatePost = (req, res, next) => {
-  const sqlUpdate = ddd;
+  //requête sql pour selectionner le status a modifier
+  const sqlSelect = "SELECT * From posts WHERE `posts`.`post_num` = ?";
+  //requête sql pour modifier le status
+  const sqlUpdate =
+    "UPDATE `posts` SET `post_post` = ? WHERE `posts`.`post_num` = ?";
+
+  const postUpdate = {
+    post_num: req.params.id,
+    post_post: req.body.post_post,
+  };
+  //recupération du status a modifier
+  db.query(sqlSelect, postUpdate.post_num, (err, results) => {
+    console.log(results);
+    if (results[0].post_id !== req.auth.userId) {
+      return res.status(403).json({ message: "ceci n'est pas votre status" });
+    } else {
+      //modification du status
+      db.execute(
+        sqlUpdate,
+        [postUpdate.post_post, postUpdate.post_num],
+        (err, results) => {
+          console.log(results);
+          if (err) {
+            return res.status(400).json(err);
+          } else {
+            return res.status(200).json({ message: "status modifier" });
+          }
+        }
+      );
+    }
+  });
 };
 
 module.exports = { getAllPost, createPost, deletePost, updatePost };
