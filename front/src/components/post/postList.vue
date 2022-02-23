4e41799656452ce927ccdf6f42acefa23c6e7d80
Théo Rosset <theo.rosset@outlook.fr>
2022-02-23 16:38:42 +0100

    boucle for déplacer sur postList

--- front/src/components/post/postList.vue
+++ front/src/components/post/postList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="postList">
     <ul class="positionPostData">
-        <posts />
+        <posts v-for="post in $store.state.posts" :post="post" :key="post.user"/>
     </ul>
   </div>
 </template>
@@ -9,12 +9,32 @@
 
 <script>
 import posts from "./posts.vue";
+import axios from "axios"
 
 export default {
   name: "postList",
+
   components: {
     posts,
   },
+  async mounted() {
+    await this.$nextTick;
+    await this.getAllPost();
+  },
+  methods:{
+     getAllPost() {
+      const token = localStorage.getItem("token");
+      axios.get("http://localhost:3000/api/groupomania/post",{
+        headers:{
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+      }).then((res) => {
+        this.$store.state.posts = res.data;
+      });
+    },
+  }
 };
 
 </script>
@@ -28,5 +48,6 @@ ul{
 .positionPostData {
   display: flex;
   justify-content: center;
+  flex-direction: column;
 }
 </style>
