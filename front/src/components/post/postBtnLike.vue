bf08e1ec32d6a93b6939189ae72a032cf4b19184
Théo Rosset <theo.rosset@outlook.fr>
2022-02-23 18:16:08 +0100

    creation du composant pour liker un post (sans les fonctionnalité)

---
+++ front/src/components/post/postBtnLike.vue
@@ -0,0 +1,84 @@
+<template>
+        <i id="likeIcon" @click="likePost(postId)" v-if="isActive === false" :postId="postId" class="far fa-thumbs-up"></i>
+        <i id="likeIcon" @click="likePost(postId)" v-else :postId="postId" class="fas fa-thumbs-up"></i>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: "like",
+  props: {
+    postId: {
+      type: Number,
+      required: true,
+    },
+  },
+  components:{
+   
+  },
+
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    likePost(postId) {
+      const token = localStorage.getItem("token");
+      const userId = localStorage.getItem("userId");
+
+      axios
+        .post(
+          `http://localhost:3000/api/groupomania/post/like/${postId}`,
+          {
+            user: userId,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then(() => {
+          this.isActive = !this.isActive;
+          
+        });
+    },
+    
+  },
+};
+
+</script>
+
+
+<style scoped>
+/* like */
+#likeIcon{
+  display: flex;
+  margin-top: 1px;
+  cursor: pointer;
+  font-size: 25px;
+}
+.background{
+    background-color: black;
+}
+
+.like{
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 17px;
+  margin-bottom: 7px;
+} 
+.likeP{
+  display: flex;
+  margin-top: 0;
+  margin-bottom: 0;
+  margin-left: 5px;
+}
+
+
+</style>
